b13c68ff4007e55129c2d01608474f7706a22912
Andrzej Prazmo <andrzej@prazmo.pl>
2024-10-15 17:25:40 +0200

    swagger

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -12,6 +12,7 @@ var webApplicationOptions = new WebApplicationOptions
 var builder = WebApplication.CreateBuilder(webApplicationOptions);
 
 builder.Services.AddEndpointsApiExplorer();
+// swagger
 builder.Services.AddSwaggerGen(context =>
 {
     context.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthenticationExample.WebApp", Version = "v1" });
